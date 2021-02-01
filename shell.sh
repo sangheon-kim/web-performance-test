e48065fb514678c77112cd450ad7e3a54b159939
sangheon-kim <ksj83671@gmail.com>
2021-02-01 21:09:31 +0900

    [김상헌] crontab 세팅중

--- shell.sh
+++ shell.sh
@@ -1,12 +1,15 @@
-#!/bin/sh
+#!/bin/bash
+
+# echo $path
 
 re="^([A-Za-z\.]{0,}(\:*\d+)*)\/*"
 
 # Please Input Your PC Web Site List
-PC_ARR=("nate.com" "news.nate.com/view/20200309n34198" "pann.nate.com/talk/353507871" "tv.nate.com/clip/4025004")
+PC_ARR=("nate.com")
+# PC_ARR=("nate.com" "news.nate.com/view/20200309n34198" "pann.nate.com/talk/353507871" "tv.nate.com/clip/4025004" "daum.net/" "naver.com/" "news.v.daum.net/v/20200309171218432" "news.naver.com/main/read.nhn?mode=LSD&mid=sec&sid1=105&oid=079&aid=0003333554")
 # Please Input Your Mobile Web Site List
 MOBILE_ARR=("m.nate.com" "m.news.nate.com/view/20200309n34198" "m.pann.nate.com/talk/353507871
-" "tv.nate.com/clip/4025004")
+" "tv.nate.com/clip/4025004" "m.daum.net" "m.naver.com" "news.v.daum.net/v/20200309171218432" "n.news.naver.com/mnews/article/079/0003333554?sid=105")
 # Today Date
 date=$(date +%Y-%m-%d)
 # throttle Folder Name  
@@ -38,14 +41,14 @@ pc_func() {
  
   touch result/$date/$throttleFolder/pc_$filename\.$ext
   
-  lighthouse https://$url \
+  $(which lighthouse) https://$url \
     --chrome-flags="--headless" \
     --output=$ext \
     --output-path=./result/$date/$throttleFolder/pc_$filename.$ext \
     --screenEmulation.disabled \
     --no-emulatedUserAgent \
     --form-factor=desktop \
-    --only-categories=performance,best-practices,accessibility,seo \
+    --only-categories=performance \
     --throttling.cpuSlowdownMultiplier=$default_throttling_pc \
     --throttling-method=devtools \
     --throttling.requestLatencyMs=40 \
@@ -132,12 +135,12 @@ for pc in "${PC_ARR[@]}"; do
     pc_no_throttling_func $pc
 done
 
-for mobile in "${MOBILE_ARR[@]}"; do
-  mobile_func $mobile
-done
+# for mobile in "${MOBILE_ARR[@]}"; do
+#   mobile_func $mobile
+# done
 
-for mobile in "${MOBILE_ARR[@]}"; do
-  mobile_no_throttling_func $mobile
-done
+# for mobile in "${MOBILE_ARR[@]}"; do
+#   mobile_no_throttling_func $mobile
+# done
 
-npm run start
+$(which npm) run start
