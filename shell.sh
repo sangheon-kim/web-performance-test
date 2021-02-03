f38f8df27266bd6804656dd228ed603224b3c6ae
sangheon-kim <ksj83671@gmail.com>
2021-02-03 18:41:41 +0900

    [김상헌] EXPORT PATH ERROR...

--- shell.sh
+++ shell.sh
@@ -3,7 +3,8 @@
 # echo $path
 
 re="^([A-Za-z\.]{0,}(\:*\d+)*)\/*"
-
+# echo $path
+export PATH=${PATH}
 # Please Input Your PC Web Site List
 PC_ARR=("nate.com")
 # PC_ARR=("nate.com" "news.nate.com/view/20200309n34198" "pann.nate.com/talk/353507871" "tv.nate.com/clip/4025004" "daum.net/" "naver.com/" "news.v.daum.net/v/20200309171218432" "news.naver.com/main/read.nhn?mode=LSD&mid=sec&sid1=105&oid=079&aid=0003333554")
