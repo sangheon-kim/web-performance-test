3b3f947b76539607df9b8d4807235d8c96d76fe6
sangheon-kim <ksj83671@gmail.com>
2021-02-15 10:13:38 +0900

    [김상헌] 코드 수정

--- shell.sh
+++ shell.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-# echo $path
-
 re="^([A-Za-z\.]{0,}(\:*\d+)*)\/*"
-# echo $path
+
 export PATH=${PATH}
 # Please Input Your PC Web Site List
-PC_ARR=("nate.com")
-# PC_ARR=("nate.com" "news.nate.com/view/20200309n34198" "pann.nate.com/talk/353507871" "tv.nate.com/clip/4025004" "daum.net/" "naver.com/" "news.v.daum.net/v/20200309171218432" "news.naver.com/main/read.nhn?mode=LSD&mid=sec&sid1=105&oid=079&aid=0003333554")
+PC_ARR=("nate.com" "news.nate.com/view/20200309n34198" "pann.nate.com/talk/353507871" "tv.nate.com/clip/4025004" "daum.net/" "naver.com/" "news.v.daum.net/v/20200309171218432" "news.naver.com/main/read.nhn?mode=LSD&mid=sec&sid1=105&oid=079&aid=0003333554")
 # Please Input Your Mobile Web Site List
 MOBILE_ARR=("m.nate.com" "m.news.nate.com/view/20200309n34198" "m.pann.nate.com/talk/353507871
 " "tv.nate.com/clip/4025004" "m.daum.net" "m.naver.com" "news.v.daum.net/v/20200309171218432" "n.news.naver.com/mnews/article/079/0003333554?sid=105")
@@ -136,12 +133,12 @@ for pc in "${PC_ARR[@]}"; do
     pc_no_throttling_func $pc
 done
 
-# for mobile in "${MOBILE_ARR[@]}"; do
-#   mobile_func $mobile
-# done
+for mobile in "${MOBILE_ARR[@]}"; do
+  mobile_func $mobile
+done
 
-# for mobile in "${MOBILE_ARR[@]}"; do
-#   mobile_no_throttling_func $mobile
-# done
+for mobile in "${MOBILE_ARR[@]}"; do
+  mobile_no_throttling_func $mobile
+done
 
 $(which npm) run start
