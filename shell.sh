868b66b165e0ee60c6294f05387adc822514b113
sangheon-kim <ksj83671@gmail.com>
2021-02-17 09:30:32 +0900

    [김상헌] 셸 스크립트 도메인 수정

--- shell.sh
+++ shell.sh
@@ -4,10 +4,11 @@ re="^([A-Za-z\.]{0,}(\:*\d+)*)\/*"
 
 export PATH=${PATH}
 # Please Input Your PC Web Site List
-PC_ARR=("nate.com" "news.nate.com/view/20200309n34198" "pann.nate.com/talk/353507871" "tv.nate.com/clip/4025004" "daum.net/" "naver.com/" "news.v.daum.net/v/20200309171218432" "news.naver.com/main/read.nhn?mode=LSD&mid=sec&sid1=105&oid=079&aid=0003333554")
+# PC_ARR=("nate.com" "news.nate.com/view/20200309n34198" "pann.nate.com/talk/353507871" "tv.nate.com/clip/4025004" "daum.net/" "naver.com/" "news.v.daum.net/v/20200309171218432" "news.naver.com/main/read.nhn?mode=LSD&mid=sec&sid1=105&oid=079&aid=0003333554")
+PC_ARR=("nate.com" "news.nate.com/view/20200309n34198" "pann.nate.com/talk/353507871" "tv.nate.com/clip/4025004")
 # Please Input Your Mobile Web Site List
 MOBILE_ARR=("m.nate.com" "m.news.nate.com/view/20200309n34198" "m.pann.nate.com/talk/353507871
-" "tv.nate.com/clip/4025004" "m.daum.net" "m.naver.com" "news.v.daum.net/v/20200309171218432" "n.news.naver.com/mnews/article/079/0003333554?sid=105")
+" "tv.nate.com/clip/4025004")
 # Today Date
 date=$(date +%Y-%m-%d)
 # throttle Folder Name  
